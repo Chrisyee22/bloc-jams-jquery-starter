aa9642a759f79b3264f72e75886615fa1fa20562
Christopher Yee <chris.anthony.yee@gmail.com>
2017-09-22 12:36:14 -0700

    bloc jams play bar

---
+++ scripts/player-bar.js
@@ -0,0 +1,25 @@
+$(document ).ready(function(){
+  $('button#play-pause').click(function(){
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
+  $('button#next').click(function(){
+    if(player.playState !=='playing'){ return;}
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) {return;}
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+
+  });
+  $('button#previous').click(function(){
+    if(player.playState !=='playing'){ return;}
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    console.log('prev', previousSongIndex,'songs', album.songs.length);
+    if (previousSongIndex < 0) {return;}
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+
+  });
+});
